d3ac6bfb813b5e184936e161285ff3dc0aca6201
Victor Popescu <19798302+vicbgdn@users.noreply.github.com>
2019-12-09 12:10:04 +0200

    Added the initial project files.

---
+++ ComplementDrugSearch/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ComplementDrugSearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
